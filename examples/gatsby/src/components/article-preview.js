44fc1dcbfec00ba32ecdc34d9aa8e3496ea794c5
denkristoffer <denkristoffer@users.noreply.github.com>
2022-05-12 09:57:31 +0200

    feat: add gatsby blog

---
+++ examples/gatsby/src/components/article-preview.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import { StaticImage } from 'gatsby-plugin-image'
+import { renderRichText } from 'gatsby-source-contentful/rich-text'
+
+import Container from './container'
+import Tags from './tags'
+import * as styles from './article-preview.module.css'
+
+const ArticlePreview = ({ posts }) => {
+  if (!posts) return null
+  if (!Array.isArray(posts)) return null
+
+  return (
+    <Container>
+      <ul className={styles.articleList}>
+        {posts.map((post) => {
+          return (
+            <li key={post.slug}>
+              <Link to={`/blog/${post.slug}`} className={styles.link}>
+                <StaticImage
+                  alt=""
+                  src="https://images.unsplash.com/photo-1538685634737-24b83e3fa2f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8"
+                />
+                <h2 className={styles.title}>{post.title}</h2>
+              </Link>
+              <div>
+                {post.description?.raw && renderRichText(post.description)}
+              </div>
+              <div className={styles.meta}>
+                <small className="meta">{post.publishDate}</small>
+                <Tags tags={post.tags} />
+              </div>
+            </li>
+          )
+        })}
+      </ul>
+    </Container>
+  )
+}
+
+export default ArticlePreview
---
+++ examples/gatsby/src/pages/index.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import get from 'lodash/get'
+
+import Layout from '../components/layout'
+import Hero from '../components/hero'
+import ArticlePreview from '../components/article-preview'
+
+class RootIndex extends React.Component {
+  render() {
+    const posts = get(this, 'props.data.allContentfulBlogPost.nodes')
+    const [author] = get(this, 'props.data.allContentfulPerson.nodes')
+
+    return (
+      <Layout location={this.props.location}>
+        <Hero
+          image
+          title={author.name}
+          content={author.shortBio}
+        />
+        <ArticlePreview posts={posts} />
+      </Layout>
+    )
+  }
+}
+
+export default RootIndex
+
+export const pageQuery = graphql`
+  query HomeQuery {
+    allContentfulBlogPost(sort: { fields: [publishDate], order: DESC }) {
+      nodes {
+        title
+        slug
+        publishDate(formatString: "MMMM Do, YYYY")
+        tags
+        description {
+          raw
+        }
+      }
+    }
+    allContentfulPerson(
+      filter: { contentful_id: { eq: "15jwOBqpxqSAOy2eOO4S0m" } }
+    ) {
+      nodes {
+        name
+        shortBio {
+          raw
+        }
+        title
+      }
+    }
+  }
+`
