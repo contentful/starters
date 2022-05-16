79870260b797401a2e2a683332e1b2d78247fd87
burakukula <trycjapa@gmail.com>
2022-05-16 10:13:50 +0200

    feat: add nextjs template

---
+++ examples/nexjs/components/hero-post.js
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import Avatar from "../components/avatar";
+import DateComponent from "../components/date";
+import ContentfulImage from "./contentful-image";
+import cn from "classnames";
+
+export default function HeroPost({ title, date, excerpt, author, slug }) {
+  return (
+    <section>
+      <div className="flex justify-center mb-8 w-full">
+        <ContentfulImage
+          width={1496}
+          height={1027}
+          alt={`Cover Image for ${title}`}
+          className={cn("col-span-2", {
+            "hover:shadow-medium transition-shadow duration-200": slug,
+          })}
+          src="https://images.unsplash.com/photo-1524134132825-dbe30ff69879?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8"
+        />
+      </div>
+      <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
+        <div>
+          <h3 className="mb-4 text-4xl lg:text-6xl leading-tight">
+            <Link href={`/posts/${slug}`}>
+              <a className="hover:underline">{title}</a>
+            </Link>
+          </h3>
+          <div className="mb-4 md:mb-0 text-lg">
+            <DateComponent dateString={date} />
+          </div>
+        </div>
+        <div>
+          <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
+          {author && (
+            <Avatar
+              name={author.name}
+              picture={{
+                url: "https://images.unsplash.com/photo-1529688530647-93a6e1916f5f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
+              }}
+            />
+          )}
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ examples/nexjs/components/more-stories.js
@@ -0,0 +1,26 @@
+import PostPreview from "../components/post-preview";
+
+export default function MoreStories({ posts }) {
+  return (
+    <section>
+      <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
+        More Stories
+      </h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
+        {posts.map((post) => (
+          <PostPreview
+            key={post.slug}
+            title={post.title}
+            coverImage={{
+              url: "https://images.unsplash.com/photo-1538685634737-24b83e3fa2f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
+            }}
+            date={post.date}
+            author={post.author}
+            slug={post.slug}
+            excerpt={post.excerpt}
+          />
+        ))}
+      </div>
+    </section>
+  );
+}
