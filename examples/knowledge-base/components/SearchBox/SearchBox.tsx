99968c1f750b7673770734738c252b13d7080ec7
Renato Massao Yonamine <massao.yonamine@contentful.com>
2022-06-22 16:29:48 +0200

    feat: make matched terms on content bold

--- examples/knowledge-base/components/SearchBox/SearchBox.tsx
+++ examples/knowledge-base/components/SearchBox/SearchBox.tsx
@@ -15,9 +15,11 @@ const styles = {
 
 export const SearchBox = () => {
   const [results, setResults] = useState<ResultType[]>([]);
+  const [query, setQuery] = useState('');
   const router = useRouter();
 
   const handleInputValueChange = async (value) => {
+    setQuery(value);
     if (!value.length) {
       setResults([]);
       return;
@@ -36,25 +38,37 @@ export const SearchBox = () => {
     router.push(item.slug);
   };
 
-  const renderResult = (result: ResultType) => (
-    <Flex flexDirection="column">
-      <Text
-        fontSize="fontSizeM"
-        lineHeight="lineHeightM"
-        fontWeight="fontWeightDemiBold"
-        className={styles.searchResults}
-      >
-        {result.title}
-      </Text>
-      <Text
-        fontSize="fontSizeS"
-        lineHeight="lineHeightS"
-        className={styles.searchResults}
-      >
-        {result.content}
-      </Text>
-    </Flex>
-  );
+  const renderResult = (result: ResultType) => {
+    const getContent = (content: string) => {
+      const queryRegexp = new RegExp(query, 'i');
+      const startIndex = content.search(queryRegexp);
+      return [
+        content.slice(0, startIndex),
+        <b>{content.slice(startIndex, startIndex+query.length)}</b>,
+        content.slice(startIndex + query.length),
+      ]
+    }
+
+
+    return (
+      <Flex flexDirection="column">
+        <Text
+          fontSize="fontSizeM"
+          lineHeight="lineHeightM"
+          fontWeight="fontWeightDemiBold"
+          className={styles.searchResults}
+        >
+          {result.title}
+        </Text>
+        <Text
+          fontSize="fontSizeS"
+          lineHeight="lineHeightS"
+          className={styles.searchResults}
+        >
+          {getContent(result.content)}
+        </Text>
+      </Flex>
+    )};
 
   return (
     <Autocomplete
