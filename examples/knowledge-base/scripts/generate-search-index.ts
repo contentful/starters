bc912d84057338c763ce9a801efc257ecefe1be1
Renato Massao Yonamine <massao.yonamine@contentful.com>
2022-06-23 12:21:44 +0200

    refactor: rename folder

--- examples/knowledge-base/scripts/generate-search-index.ts
+++ examples/knowledge-base/scripts/generate-search-index.ts
@@ -7,8 +7,7 @@ async function run() {
   const index = await buildSearchIndex();
   const serializedIndex = JSON.stringify(index);
 
-  console.log({path: path.resolve('files', "searchIndex.json")})
-  await fs.writeFile(path.resolve('files', "searchIndex.json"), serializedIndex, {
+  await fs.writeFile(path.resolve('static', "searchIndex.json"), serializedIndex, {
     encoding: "utf-8",
   });
 }
